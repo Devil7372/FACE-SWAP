f4e3bf9841c74b8b4953fcc313f530a7090f7551
Devil7372 <devilgamingofficial59@gmail.com>
2025-07-15 10:55:48 +0530

    Update api_swap.py

--- utils/api_swap.py
+++ utils/api_swap.py
@@ -1,8 +1,16 @@
-import os
 def swap_faces(path1, path2):
-    # Dummy swap: just return first image as "result"
-    result_path = f"{os.path.splitext(path1)[0]}_swapped.jpg"
-    with open(path1, 'rb') as fsrc, open(result_path, 'wb') as fdst:
-        fdst.write(fsrc.read())
-    return result_path
-# For real usage, add API request code here.
+    url = "https://your-api.com/face-swap"
+    with open(path1, 'rb') as f1, open(path2, 'rb') as f2:
+        files = {
+            'image1': f1,
+            'image2': f2
+        }
+        response = requests.post(url, files=files)
+
+    if response.status_code == 200:
+        result_path = f"{os.path.splitext(path1)[0]}_swapped.jpg"
+        with open(result_path, 'wb') as out_file:
+            out_file.write(response.content)
+        return result_path
+    else:
+        raise Exception(f"API Error: {response.status_code} - {response.text}")
